e13cd57e7912d46f9d86ab024835863d4fbfd83e
TimonJasarevic <timonhdmc@gmail.com>
2026-06-17 21:48:01 +0200

    Fixed setup

--- src/misinfo_abm/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Misinformation diffusion ABM."""
-
-from misinfo_abm.config import ModelConfig
-from misinfo_abm.model import MisinformationModel
-
-__all__ = ["ModelConfig", "MisinformationModel"]
